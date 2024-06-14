cf1c990a9ab660f28a1ae800c94258a6bbb8cf2f
Marco Maddo <profmaddo@gmail.com>
2024-06-14 15:15:06 +0000

    Contatenar dois dados diferentes

---
+++ contat_dados.py
@@ -0,0 +1,15 @@
+# vamos receber dois dados diferentes do usuário 
+# e contatena-los em uma única string
+
+
+# Solicita a entrada do primeiro dado
+primeiroDado = input("Digite o primeiro dado: ")
+
+# Solicita a entrada do segundo dado
+segundoDado = input("Digite o segundo dado: ")
+
+# Concatena os dados com um espaço entre eles
+dadosConcatenados = primeiroDado + " " + segundoDado
+
+# Exibe a nova frase
+print("Você digitou:", dadosConcatenados)
